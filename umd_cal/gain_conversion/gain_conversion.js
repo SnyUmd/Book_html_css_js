babf196551abe74363128dd483104e954de23a0b
DESKTOP-BL\SNY-Beelink <sny.umd@gmail.com>
2021-12-24 09:37:56 +0900

    gain換算追加

--- umd_cal/gain_conversion/gain_conversion.js
+++ umd_cal/gain_conversion/gain_conversion.js
@@ -1,42 +1,32 @@
-function OrmCalc(typ, id0, id1, rsl_id){
-    result = 0;
-    a = parseFloat(document.getElementById(id0).value);
-    b = parseFloat(document.getElementById(id1).value);
-    switch(typ)
-    {
-        case 1://電流計算
-        case 2://抵抗値計算
-            // result = document.getElementById(id0).value / document.getElementById(id1).value;
-            result = a / b;
-            break;
-        case 3://電圧計算
-            // result = document.getElementById(id0).value * document.getElementById(id1).value;
-            result = a * b;
-            break;
-    }
-    //alert(result);
-    document.getElementById(rsl_id).value = result;
-}
+stdValtage = 1;
+stdWatt = 0.001;
 
-function WattCalc(typ, id0, id1, rsl_id){
-    result = 0;
-    a = parseFloat(document.getElementById(id0).value);
-    b = parseFloat(document.getElementById(id1).value);
-    switch(typ)
-    {
-        case 1://電圧抵抗
-            // result = Math.pow(document.getElementById(id0).value,2) / document.getElementById(id1).value;
-            result = Math.pow(a, 2) / b;
-            break;
-        case 2://電圧電流
-            // result = document.getElementById(id0).value * document.getElementById(id1).value;
-            result = a * b;
-            break;
-        case 3://電流抵抗
-            // result = document.getElementById(id0).value * Math.pow(document.getElementById(id1).value, 2);
-            result = a * Math.pow(b, 2);
-            break;
-    }
-    //alert(result);
-    document.getElementById(rsl_id).value = result;
+function Calc(typ, id0, rsl_id0, rsl_id1){
+  result0 = 0;
+  result1 = 0;
+  a = parseFloat(document.getElementById(id0).value);
+  // b = parseFloat(document.getElementById(id1).value);
+
+  switch(typ)
+  {
+      case 11://倍率⇒電圧値
+          result0 = a * stdValtage;
+          result1 = 20 * Math.log10(a);
+          break;
+      case 12://電圧計算
+          result1 = Math.pow(10, (a / 20));
+          result0 = result1 * stdValtage;
+          break;
+      case 21:
+          result0 = a * stdWatt * 1000;
+          result1 = 10 * Math.log10(a);
+          break;
+      case 22:
+          result1 = Math.pow(10, (a / 10));
+          result0 = result1 * stdWatt * 1000;
+          break;
+  }
+  //alert(result);
+  document.getElementById(rsl_id0).value = result0;
+  document.getElementById(rsl_id1).value = result1;
 }
